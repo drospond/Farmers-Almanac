b31aa8c7c56284d815f19ac93f0574fc0fd441e7
drospond <david.rospond@gmail.com>
2020-07-02 00:18:26 -0400

    initialized home page

---
+++ client/src/Containers/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the digital Farmer's Almanac</h1>
+                <h2>Easy guide to plan you home garden</h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
